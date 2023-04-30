efdb9672b1fe47a228ed4b23fd6089065bfdd726
mimzy <tzachi@diysimchas.com>
2023-04-30 11:22:31 -0400

    go deeper for for accurate info

--- access-a-ride.js
+++ access-a-ride.js
@@ -3,15 +3,16 @@ const {chromium} = require('playwright');
 dotenv = require('dotenv');
 dotenv.config();
 
-const MTA_USERNAME = process.env.MTA_USERNAME
+const MTA_USERNAME = process.env.MTA_USERNAME;
 const MTA_PASSWORD = process.env.MTA_PASSWORD;
+const HEADLESS = process.env.NODE_ENV === 'production' ? true : false;
 
 /**
  * Get authentication tokens from MTA website, which is a React application
  */
 async function getAuthenticatedChromium(){
     const loginPage = 'https://aar.mta.info/login';
-    const browser = await chromium.launch({headless: true});
+    const browser = await chromium.launch({headless: HEADLESS});
     const page = await browser.newPage();
 
     // Navigate to login page
@@ -33,6 +34,13 @@ async function getAuthenticatedChromium(){
     return {browser, page};
 }
 
+
+/**
+ * Obtain the schedule form the trip-dashboard component. If trip details are available, 
+ * obtain and return that instead since it contains more accurate information.
+ * 
+ * @returns 
+ */
 async function getSchedule(){
     const url = 'https://aar.mta.info/trip-booking';
         const selector = "div[class=trip-dashboard]"
@@ -70,12 +78,33 @@ async function getSchedule(){
             $ = cheerio.load(html);
             schedule = $(selector).html();
         }
-        
-        await page.close();
-        await browser.close();
 
         // replace any relative links with absolute links
         schedule = schedule.replace(/href="\//g, 'href="https://aar.mta.info/');
+        
+        // check the schedule object, which is html text, for an anchor element with inner text "See trip details"
+        // if it exists, navigate to the href attribute and get the html text from that page
+        const anchorSelector = "a:contains('See trip details')";
+        const anchor = cheerio.load(schedule)(anchorSelector);
+        if(anchor.length > 0){
+            const href = anchor.attr('href');
+            console.log("\n\n see details href:\n", href);
+            if(href.length > 0){
+                await page.goto(href);
+                await page.waitForLoadState('networkidle');
+                await page.screenshot({path: `${process.env.HOME}/Downloads/see-details.png`});
+                html = await page.content();
+                $ = cheerio.load(html);
+                const itinerarySelector = "div[class=itinerary]";
+                schedule = $(itinerarySelector).html();
+                schedule = schedule.replace(/href="\//g, 'href="https://aar.mta.info/');
+                
+            }
+            
+        }
+
+        await page.close();
+        await browser.close();
         return schedule;
 
 }
