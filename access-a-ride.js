4d73029d9dbae103e2c200da09ac7e117e2d14e5
mimzy <tzachi@diysimchas.com>
2023-04-30 11:26:49 -0400

    format

--- access-a-ride.js
+++ access-a-ride.js
@@ -1,5 +1,5 @@
 const cheerio = require('cheerio');
-const {chromium} = require('playwright');
+const { chromium } = require('playwright');
 dotenv = require('dotenv');
 dotenv.config();
 
@@ -8,7 +8,7 @@ const MTA_PASSWORD = process.env.MTA_PASSWORD;
 const HEADLESS = process.env.NODE_ENV === 'production' ? true : false;
 
 
-function makeRelativeLinksAbsolute(html){
+function makeRelativeLinksAbsolute(html) {
     // replace any relative links with absolute links
     return html.replace(/href="\//g, 'href="https://aar.mta.info/');
 }
@@ -16,30 +16,30 @@ function makeRelativeLinksAbsolute(html){
 /**
  * Get authentication tokens from MTA website, which is a React application
  */
-async function getAuthenticatedChromium(){
+async function getAuthenticatedChromium() {
     const loginPage = 'https://aar.mta.info/login';
-    const browser = await chromium.launch({headless: HEADLESS});
+    const browser = await chromium.launch({ headless: HEADLESS });
     const page = await browser.newPage();
 
     // Navigate to login page
     await page.goto(loginPage);
     await page.waitForLoadState('networkidle');
 
-    if(!HEADLESS)
-        await page.screenshot({path: `${process.env.HOME}/Downloads/pre-login.png`});
-    
+    if (!HEADLESS)
+        await page.screenshot({ path: `${process.env.HOME}/Downloads/pre-login.png` });
+
     // Enter username and password and submit
     await page.type(`input[name="username"]`, MTA_USERNAME);
     await page.type(`input[name="password"]`, MTA_PASSWORD);
 
     // Click the login button, which is a button with type="submit"
     await page.click(`button[type="submit"]`);
-    
+
     // return the authenticated browser and the page
-    if(!HEADLESS)
-        await page.screenshot({path: `${process.env.HOME}/Downloads/post-login.png`});
-    
-    return {browser, page};
+    if (!HEADLESS)
+        await page.screenshot({ path: `${process.env.HOME}/Downloads/post-login.png` });
+
+    return { browser, page };
 }
 
 
@@ -49,76 +49,76 @@ async function getAuthenticatedChromium(){
  * 
  * @returns 
  */
-async function getSchedule(){
+async function getSchedule() {
     const url = 'https://aar.mta.info/trip-booking';
-        const selector = "div[class=trip-dashboard]"
+    const selector = "div[class=trip-dashboard]"
 
-        const {browser, page} = await getAuthenticatedChromium();
-        // await page.goto(url);
-        // await page.waitForLoadState('networkidle');
+    const { browser, page } = await getAuthenticatedChromium();
+    // await page.goto(url);
+    // await page.waitForLoadState('networkidle');
 
-        if(!HEADLESS)
-            await page.screenshot({path: `${process.env.HOME}/Downloads/pre-cheerio.png`});
-    
-        // get page's url
-        let pageUrl = page.url();
+    if (!HEADLESS)
+        await page.screenshot({ path: `${process.env.HOME}/Downloads/pre-cheerio.png` });
+
+    // get page's url
+    let pageUrl = page.url();
+    console.log("\n\n Page URL:\n", pageUrl);
+
+    while (pageUrl !== url) {
+        console.log("whilin' out");
+        await page.waitForLoadState('networkidle');
+        if (!HEADLESS)
+            await page.screenshot({ path: `${process.env.HOME}/Downloads/finally-idle.png` });
+        pageUrl = page.url();
         console.log("\n\n Page URL:\n", pageUrl);
-        
-        while(pageUrl !== url){
-            console.log("whilin' out");
-            await page.waitForLoadState('networkidle');
-            if(!HEADLESS)
-                await page.screenshot({path: `${process.env.HOME}/Downloads/finally-idle.png`});
-            pageUrl = page.url();
-            console.log("\n\n Page URL:\n", pageUrl);
-        }
+    }
 
-        let html = await page.content();
-        let $ = cheerio.load(html);
+    let html = await page.content();
+    let $ = cheerio.load(html);
 
-        // Use Cheerio selectors to extract the schedule data
-        let schedule = $(selector).html();
-        // WHile schedule contains the word "Loading", wait for the page to load
-        while(schedule.includes("Loading")){
-            console.log("whilin' out again");
+    // Use Cheerio selectors to extract the schedule data
+    let schedule = $(selector).html();
+    // WHile schedule contains the word "Loading", wait for the page to load
+    while (schedule.includes("Loading")) {
+        console.log("whilin' out again");
+        await page.waitForLoadState('networkidle');
+        if (!HEADLESS)
+            await page.screenshot({ path: `${process.env.HOME}/Downloads/finally-idle2.png` });
+        html = await page.content();
+        $ = cheerio.load(html);
+        schedule = $(selector).html();
+    }
+
+    // replace any relative links with absolute links
+    schedule = makeRelativeLinksAbsolute(schedule);
+
+    // check the schedule object, which is html text, for an anchor element with inner text "See trip details"
+    // if it exists, navigate to the href attribute and get the html text from that page
+    const anchorSelector = "a:contains('See trip details')";
+    const anchor = cheerio.load(schedule)(anchorSelector);
+    if (anchor.length > 0) {
+        const href = anchor.attr('href');
+        console.log("\n\n see details href:\n", href);
+        if (href.length > 0) {
+            await page.goto(href);
             await page.waitForLoadState('networkidle');
-            if(!HEADLESS)
-                await page.screenshot({path: `${process.env.HOME}/Downloads/finally-idle2.png`});
+            if (!HEADLESS)
+                await page.screenshot({ path: `${process.env.HOME}/Downloads/see-details.png` });
             html = await page.content();
             $ = cheerio.load(html);
-            schedule = $(selector).html();
+            const itinerarySelector = "div[class=itinerary]";
+            schedule = $(itinerarySelector).html();
+            schedule = makeRelativeLinksAbsolute(schedule);
+
         }
 
-        // replace any relative links with absolute links
-        schedule = makeRelativeLinksAbsolute(schedule);
-        
-        // check the schedule object, which is html text, for an anchor element with inner text "See trip details"
-        // if it exists, navigate to the href attribute and get the html text from that page
-        const anchorSelector = "a:contains('See trip details')";
-        const anchor = cheerio.load(schedule)(anchorSelector);
-        if(anchor.length > 0){
-            const href = anchor.attr('href');
-            console.log("\n\n see details href:\n", href);
-            if(href.length > 0){
-                await page.goto(href);
-                await page.waitForLoadState('networkidle');
-                if(!HEADLESS)
-                    await page.screenshot({path: `${process.env.HOME}/Downloads/see-details.png`});
-                html = await page.content();
-                $ = cheerio.load(html);
-                const itinerarySelector = "div[class=itinerary]";
-                schedule = $(itinerarySelector).html();
-                schedule = makeRelativeLinksAbsolute(schedule);
-                
-            }
-            
-        }
+    }
 
-        await page.close();
-        await browser.close();
-        return schedule;
+    await page.close();
+    await browser.close();
+    return schedule;
 
 }
-    
 
-module.exports.getSchedule =  getSchedule;
+
+module.exports.getSchedule = getSchedule;
