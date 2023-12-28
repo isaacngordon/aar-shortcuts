a677e2d21cebc215102a5140f4520e205f8bfdbb
mimzy <tzachi@diysimchas.com>
2023-12-27 21:37:16 -0500

    expire tomorrow

--- routers/yogo.js
+++ routers/yogo.js
@@ -4,7 +4,16 @@ const path = require('path');
 const router = express.Router();
 
 // Serve static files from the "../yogo" directory
-router.use('/', express.static(path.join(__dirname, '..', 'yogo')));
-
+router.use('/', (req, res, next) => {
+    const currentDate = new Date();
+    const targetDate = new Date('December 28, 2023');
+    
+    if (currentDate >= targetDate) {
+        res.status(410);
+        res.send('Expired link');
+    } else {
+        express.static(path.join(__dirname, '..', 'yogo'))(req, res, next);
+    }
+});
 
 module.exports = router;
